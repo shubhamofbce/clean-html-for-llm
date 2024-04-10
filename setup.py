339b2eedfa5e7638a7a1149b2af97037b8d9dbca
shubhamofbce <sraj13169@gmail.com>
2024-04-10 14:27:26 +0530

    Update readme and add workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clean_html_for_llm',
-    version='1.3.0',
+    version='1.3.1',
     description='A library for cleaning HTML content by removing specified tags and attributes.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
